26c3b45e16e8d13df403110a6b5546cc289262b2
Eldon-NCYG <71125829+Eldon-NCYG@users.noreply.github.com>
2022-05-11 20:52:20 +1200

    Add files via upload

---
+++ c1_sign_up.py
@@ -0,0 +1,38 @@
+def login(username, password):
+    print("Logged in", username, password)
+
+
+def register(username, password):
+    print("Registered", username, password)
+
+
+def log_or_reg(option):
+    if option == "l":
+        username = input("\nUsername: ")
+        password = input("Password: ")
+        login(username, password)
+    else:
+        print("\nCreate your username and password to register.")
+        username = input("Username: ")
+        password = input("Password: ")
+        confirm_password = input("Confirm Password: ")
+
+        if password == confirm_password:
+            register(username, password)
+        else:
+            print("\nError: Passwords don't match up.")
+            log_or_reg(option)
+
+
+def start():
+    global option
+    print("\nWelcome to the BDSC Uniform Shop!")
+    option = input("Login or Register (L / R): ")
+    option = option.strip().lower()
+    if option != "l" and option != "r":
+        print("Please enter a valid input.\n")
+        start()
+
+
+start()
+log_or_reg(option)
