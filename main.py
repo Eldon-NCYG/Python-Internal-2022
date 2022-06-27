50511277c06ea7b0f683c5d9f9b7e05266046b62
Eldon Chan <71125829+Eldon-NCYG@users.noreply.github.com>
2022-06-27 12:20:59 +0000

    27/6/22

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 #Importing the uniform product list from external file
 from product_list import male_list, female_list, unisex_list
-from checkout import final
 
-#Error Handling for user input of numbers
+#Global Error Handling for user input of numbers
 def inputNumber(question, message):
   while True:
     try:
@@ -20,11 +19,12 @@ def inputNumber(question, message):
 
 
 
-#Login compoennt
+#Component 2: Login System ------------------------------
 def login(username, password):
-
+  
   logged_in = False
 
+  #Checking if the username & password that has been input by the user is inside the database
   file = open("accounts.txt", "r")
   for i in file:
       a,b = i.split(",")
@@ -34,20 +34,25 @@ def login(username, password):
           break
   file.close()
 
+  #If the username & password exists in the database, the main main function of the program starts up and the user logs in. 
   if logged_in:
       print("Successfully logged in as " + username)
       main(username)
   else:
+      #Invalid username and password inputted by the user
       print("Incorrect Username or Password. Please Try Again.")
       log_or_reg(option)
 
       
-#Register account component
+#Component 1: Register account ---------------------------------------
 def register(username, password):
+  #Adding the newly registered username & password inputted by the user into the accounts databse
   file = open("accounts.txt", "a")
   file.write("\n" + username + "," + password)
   file.close()
   print("Registered " + username)
+
+  #After registering, the main function starts up 
   main(username)
 
 #Allowing user to login or register depending on their option
@@ -56,7 +61,7 @@ def log_or_reg(option):
   username = ""
   password = ""
 
-  #Entering account details for Login Component
+  #After choosing to login, user can input existing username & password to pass into the login component
   if option == "l":
     print("\nPlease enter your username and password to log in.\nEnter nothing in the username and password to go back.")
     username = input("Username: ")
@@ -67,8 +72,11 @@ def log_or_reg(option):
       exit()
     else:
       login(username, password)
+
+  #if the user chooses to register, they can input details to pass into teh regsiter component
   elif option == "r":
-    #Register account component
+    
+    #Entering details to register an account
     print("\nCreate your username and password to register.\nEnter nothing in the username and password to go back.")
     username = input("Username: ")
     password = input("Password: ")
@@ -80,14 +88,13 @@ def log_or_reg(option):
     age = inputNumber("Enter your age: ", "age")
   
 
-            
+    #Checking if the user's account details meet the requirements (long enough password length, age between 13-18, etc)     
     if len(password) >= 10:
       if password == confirm_password:
         if age >= 13 and age <= 18:
           register(username, password)
         else:
-          print("Sorry, this uniform shop program is only for students of BDSC")
-          log_or_reg(option)
+          print("Sorry, this uniform shop program is only for students of BDSC (ages 13 - 18).\n\nProgram Ended")
       else:
         print("\nError: Passwords don't match up. Please Try Again.")
         log_or_reg(option)
@@ -97,6 +104,8 @@ def log_or_reg(option):
 
 
 
+#Component 3: Uniform Product List --------------------------------------------------
+      
 #Printing the category of products depending on the user's choice
 def show_products(selection):
   if selection == "male":
@@ -137,7 +146,6 @@ def add_to_cart(item, category):
   price = product["price"]
   sizeable = product["sizeable"]
 
-
   #Only ask for size if the product is sizable
   if sizeable:
     input_size()
@@ -148,41 +156,43 @@ def add_to_cart(item, category):
   add_product = {}
   add_product["name"] = name
   add_product["quantity"] = quantity
-
   add_product["price"] = quantity * price
-  
-  
-  
+
+  #If the product is sizeable, add a size property to the item
   if sizeable:
     print("Added " + str(quantity) + "x " + size.upper() + " " + name + " to shopping cart.")
     add_product["size"] = size.upper()
     shopping_cart.append(add_product)
-
+  #If teh product isn't sizeable, the size property isn't added
   else:
     print("Added " + str(quantity) + "x " + name + " to shopping cart.")
     shopping_cart.append(add_product)
 
 
  
-
+#View female products list function
 def female():
+  #Printing out all of the products in the female product list
   print("\n\nFemale Uniform Shop")
   show_products("female")
   keys = [d['key'] for d in female_list]
 
+  #User inputs a product from the female product list
   select_item = input("\nEnter in nothing to go back\nAdd item to your cart: ")
   select_item = select_item.lower().strip()
 
+  #If product inputted by user exists in the female product list, it is added to their shopping cart.
   if select_item in keys:
     add_to_cart(select_item, female_list)
     female()
+  #If user inputs nothing, the program will go back and allow the user to choose another category of products to view
   elif select_item == "":
     main(username)
   else:
-    print("\nPlease input a product's key (a, b, c, etc)")
+    print("\Error: nPlease input a valid product's key (a, b, c, etc)")
     female()
   
-
+#View male products list function
 def male():
   #Printing out all of the male products for the user to see
   print("\n\nMale Uniform Shop")
@@ -197,40 +207,108 @@ def male():
   if select_item in keys:
     add_to_cart(select_item, male_list)
     male()
-  #Option to go back to select the shop category
+  #If user inputs nothing, the program will go back and allow the user to choose another category of products to view
   elif select_item == "":
     main(username)
   #If the user enters anything that is invalid, will tell them an error has occurred and let them try again
   else:
-    print("\nPlease input a product's key (a, b, c, etc)")
+    print("\nError: Please input a product's key (a, b, c, etc)")
     male()
     
 
 
-
+#View unisex list function
 def unisex():
+  #Printing out all of the products in the unisex product list
   print("\nUnisex Uniform Shop\n")
-
   show_products("unisex")
   keys = [d['key'] for d in unisex_list]
   
   select_item = input("\nEnter in nothing to go back\nAdd item to your cart: ")
   select_item = select_item.lower().strip()
 
+  #If product inputted by user exists in the unisex product list, it is added to their shopping cart.
   if select_item in keys:
     add_to_cart(select_item, unisex_list)
     unisex()
+  #If user inputs nothing, the program will go back and allow the user to choose another category of products to view
   elif select_item == "":
     main(username)
+  #If the user enters anything that is invalid, will tell them an error has occurred and let them try again
   else:
-    print("\nPlease input a product's key (a, b, c, etc)")
+    print("\nError: Please input a product's key (a, b, c, etc)")
     unisex()
 
+    
+
+
+
+
+#Component 4: Checkout (showing all of the prodcuts in the  user's shopping cart)---
+
+#Total Price of all products in shopping cart
+def checkout_price():
+  total_price = 0
+  for product in shopping_cart:
+    total_price += product["price"] 
+
+  return total_price
+
+
+#View List of all products in shopping_cart
+def shopping_cart_list(products):
+  print("\n\nYour Shopping Cart:")
+
+  #For loop to print every product in the shopping cart
+  for product in products:
+    x = 1
+    if "size" in product.values():
+      print(str(products.index(product) + 1) + ". " +     str(product["quantity"]) + "x " + product["size"] + " " + product["name"] + ": $" + str(product["price"]))
+    else:
+      print(str(products.index(product) + 1) + ". " + str(product["quantity"]) + "x " + product["name"] + ": $" + str(product["price"]))
+    x = x + 1
+  
+
+#Remove item from shopping cart function
+def remove_item():
+  option = input("\nOption: ")
+
+  #Checking if user input a a valid product number to remove
+  if option.isdigit():
+    option = int(option)
+    option = option - 1
+    if option <= (len(shopping_cart) + 1) and option != 0:
+      print("Removed (" + str(shopping_cart[option]["quantity"]) + "x " + shopping_cart[option]["name"] + ") from your shopping cart.")
+      shopping_cart.pop(option)
+      shopping_cart_list(shopping_cart)
+      remove_item()
+
+  #Entering nothing allows user to go back a page
+  elif option == "":
+    main(username)
+  elif option.lower().strip() == "checkout":
+    print("\nTotal Price: $" + str(checkout_price()) + ".00\nHave a good day!")
+  #When the user inputs an invalid option, the program will allow the user to input again
+  else:
+    print("Error: Please enter a valid option")
+    remove_item()
+  
+#Final function that allows user to remove items from the shopping cart or fully checkout and end the program
+def final():
+  shopping_cart_list(shopping_cart)
+  #Allowing the user to remove an item from the shopping cart or go back to the shop pages or to fully checkout
+  print("\nTo remove an item, enter in the product number\nTo go back to the shopping page, enter in nothing\nTo checkout, type 'checkout'")
+  remove_item()
+
+
+  
 #Main function where user chooses 
 def main(username):
   print("\nWelcome to the BDSC Uniform Shop " + username + "\n")
   print("Would you like to look at Male, Female, or Unisex products? (M/F/U)?")
   print("To checkout, enter in 'C'")
+
+  #User can choose to view male, female, unisex, or checkout
   option = ""
   option = input("Option: ")
   option = option.lower().strip()
@@ -242,49 +320,32 @@ def main(username):
   elif option == "u":
       unisex()
   elif option == "c":
-      pass
+      final()
+  #Error handling
   else:
-      print("\nPlease choose either Male, Female, or Unisex Products.")
+      print("\nError: Please choose either Male, Female, or Unisex Products.")
       main(username)
 
 
 
 #Option for user to choose to login or register an account
 def start():
-    global option
-    print("\nWelcome to the BDSC Uniform Shop!")
-    option = input("Login or Register (L / R): ")
-    option = option.strip().lower()
-    if option != "l" and option != "r":
-        print("Please enter a valid input.\n")
-        start()
-    else:
-      log_or_reg(option)
+  global option
+  print("\nWelcome to the BDSC Uniform Shop!")
+  option = input("Login or Register (L / R): ")
+  option = option.strip().lower()
+  if option != "l" and option != "r":
+    print("Error: Please enter a valid input.\n")
+    start()
+  #Error handling
+  else:
+    log_or_reg(option)
 
             
-
+#Starting the whole program
 start()
 
 
-#Component 4: Checkout (showing all of the prodcuts in the  user's shopping cart)
-def final(products):
-  print("\n\nYour Shopping Cart:")
 
-  #For loop to print every product in the shopping cart
-  for product in products:
-    x = 1
-    if "size" in product.values():
-      print(str(products.index(product) + 1) + ". " + str(product["quantity"]) + "x " + product["size"] + " " + product["name"] + ": $" + str(product["price"]))
-    else:
-      print(str(products.index(product) + 1) + ". " + str(product["quantity"]) + "x " + product["name"] + ": $" + str(product["price"]))
-    x = x + 1
-
-    #Allowing the user to remove an item from the shopping cart or go back to the shop pages or to fully checkout
-  print("\nTo remove an item, enter in the product number\nTo go back to the shopping page, enter in nothing")
-  option = input("Option: ")
-
-
-
-final(shopping_cart)
 
 
--- test.py
+++ test.py
@@ -1,14 +1,33 @@
-def input_size():
-  global size
-  sizes = ["xs", "s", "m", "l", "xl"]
-  size = input("Size (XS, S, M L, XL): ")
-  size = size.lower().strip()
-\
-  if size not in sizes:
-    print("Please input a valid size.")
-    input_size()
-  else:
-    pass
+shopping_cart = [{'name': 'Junior Boys Short Sleeve Shirt', 'quantity': 2, 'price': 94, 'size': 'S'}, {'name': 'Senior Boys Socks', 'quantity': 5, 'price': 9, 'size': 'S'}]
 
-input_size()
-print(size)
+
+def checkout_price():
+  total_price = 0
+  for product in shopping_cart:
+    total_price += product["price"] 
+
+  return total_price
+
+
+#Remove item from shopping cart function
+def remove_item():
+  option = input("Option: ")
+  if option.isdigit():
+    option = int(option)
+    option = option - 1
+    if option <= (len(shopping_cart) + 1) and option != 0:
+      print("Removed (" + str(shopping_cart[option]["quantity"]) + "x " + shopping_cart[option]["name"] + ") from your shopping cart.")
+      print(shopping_cart[option]["quantity"])
+      shopping_cart.pop(option)
+      print(shopping_cart)
+
+    remove_item()
+  elif option == "":
+    main(username)
+  elif option.lower().strip() == "checkout":
+    print("\nTotal Price: $" + str(checkout_price()) + ".00\n Have a good day!")
+  else:
+    print("Please enter a valid option")
+    remove_item()
+
+remove_item()
